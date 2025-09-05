ed179145478d316994f03a910e270b4741b93ced
Mik3yD3 <132089717+Mik3yD3@users.noreply.github.com>
2025-09-05 10:45:21 -0500

    Create WN10-AC-000020.ps1

---
+++ STIGS/WN10-AC-000020.ps1
@@ -0,0 +1,36 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the password history must be configured to 24 remembered passwords to stop users from recycling the same passwords frequently.
+
+.NOTES
+    Author          : Michael Albert
+    LinkedIn        : linkedin.com/in/michael-albert-/
+    GitHub          : github.com/Mik3yD3
+    Date Created    : 2025-09-05
+    Last Modified   : 2025-09-05
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AC-000020
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-AC-000020.ps1 
+#>
+
+
+# Set minimum password age to 1 day
+net accounts /minpwage:1
+
+# Set password history length to 24
+net accounts /uniquepw:24
+
+# Verify
+net accounts
